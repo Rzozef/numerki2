0eb56412500c874f1bf4cc62c9459a27b88256d4
Rzozef <236533@edu.p.lodz.pl>
2022-03-29 17:40:06 +0200

    obsluga uzytkownika co az w oczy parzy

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def load_from_file(path):
     m2 = []
     for i in range(count_rows):
         for j in range(len(m1[i])):
-            m2.append(m1[i][j].rstrip(','))  # typ string, mozna rzutowac w tym miejscu na inta, ale niektore macierze maja floatygit 
+            m2.append(m1[i][j].rstrip(','))  # typ string, mozna rzutowac w tym miejscu na inta, ale niektore
+            # macierze maja floaty
     m3 = []
     matrix = []
     for i in range(count_rows):
@@ -45,7 +46,7 @@ def return_a(matrix):
     return np.asmatrix(aa)
 
 
-def is_diagonally_dominant(matrix):  # mozliwe ze sie nie przyda
+def is_diagonally_dominant(matrix):
     diag = np.diag(np.abs(matrix))
     print(diag)
     off_diag = np.sum(np.abs(matrix), axis=1)
@@ -59,23 +60,66 @@ def is_diagonally_dominant(matrix):  # mozliwe ze sie nie przyda
 
 
 def gauss_seidel_method(matrix):
-    # dorobic sprawdzanie warunkow koniecznych TODO
+    # TODO jakie byly warunki konieczne?
     b = return_b(matrix)
     a = return_a(matrix)
-    x = [0, 0, 0]  # do poprawy zeby bylo dostosowane do kazdej ilosci rownan
-    x = np.asmatrix(x)
-    x.transpose()
+    x = []
+    xmatrix = []
+    for i in range(len(b)):
+        x.append(0)
+        xmatrix.append(x.copy())
+        x.clear()
+    xmatrix = np.asmatrix(xmatrix)
+    xmatrix.transpose()
+    print(xmatrix)
     for k in range(a.shape[0]):
         b_k = b[k]
         for j in range(a.shape[1]):
             if j != k:
-                b_k -= a[k][j] * x[j]  # zly wzor
-        x[k] = b_k / a[k][k]
-    return x
+                b_k -= a[k][j] * xmatrix[j]  # zly wzor
+        xmatrix[k] = b_k / a[k][k]
+    return xmatrix
 
 
 def main():
-    print(load_from_file("r1.txt"))
+    choice_first = None
+    while choice_first is None:
+        print("W jaki sposób uzupełnić współczynniki?")
+        print("\t1. Ręcznie")
+        print("\t2. Wczytaj z pliku")
+        choice_first = input("\t\t>>> ")
+        if int(choice_first) < 1 or int(choice_first) > 2:
+            print("Nie ma takiej opcji w menu")
+            choice_first = None
+    if int(choice_first) == 1:
+        how_much = 0
+        while int(how_much) <= 0:
+            print("Ile równań chcesz wprowadzić?")
+            how_much = input("\t\t>>> ")
+        # todo wczytywanie wspolczynnikow z konsoli
+    else:
+        print("Podaj sciezke do pliku")
+        path = input("\t\t>>> ")
+        matrix = load_from_file(path)
+    stop_term = None
+    while stop_term is None:
+        print("Wybierz warunek stopu:")
+        print("\t1. Spełnienie warunku założonego przez dokładność")
+        print("\t2. Osiągnięcie zadanej liczby iteracji")
+        stop_term = input("\t\t>>> ")
+        if int(stop_term) < 1 or int(stop_term) > 2:
+            print("Nie ma takiej opcji w menu")
+            stop_term = None
+
+    epsilon, iterations = None, 0
+
+    if stop_term == "1":
+        while epsilon is None:
+            epsilon = input("Podaj epsilon: ")
+    else:
+        while iterations == 0:
+            iterations = input("Podaj maksymalną liczbę iteracji: ")
+    gauss_seidel_method(matrix)
 
 
 if __name__ == "__main__":
